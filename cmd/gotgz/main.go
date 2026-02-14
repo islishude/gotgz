eef542621e05696967e8c548b7984477d833cdd1
islishude <islishude@gmail.com>
2026-02-14 21:23:41 +0800

    Refactor main function to measure and log execution time on successful completion

--- cmd/gotgz/main.go
+++ cmd/gotgz/main.go
@@ -7,6 +7,7 @@ import (
 	"os/signal"
 	"path/filepath"
 	"syscall"
+	"time"
 
 	"github.com/islishude/gotgz/internal/cli"
 	"github.com/islishude/gotgz/internal/engine"
@@ -33,9 +34,13 @@ func main() {
 		os.Exit(engine.ExitFatal)
 	}
 
+	start := time.Now()
 	result := runner.Run(basectx, opts)
 	if result.Err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "gotgz: %v\n", result.Err)
+	}
+	if result.ExitCode == engine.ExitSuccess {
+		_, _ = fmt.Fprintf(os.Stderr, "gotgz: completed in %s\n", time.Since(start).Round(time.Millisecond))
 	}
 	os.Exit(result.ExitCode)
 }
